fafab117eb8701aea3f30bcfa393f129720e0c70
PayalMalawalkar1234 <111423241+PayalMalawalkar1234@users.noreply.github.com>
2022-11-21 23:28:47 +0530

    Add files via upload

---
+++ Practicals/CountryTableMySQL/HibernateExample/src/main/java/com/many/stock/GeneratedValue.java
@@ -0,0 +1,7 @@
+package com.many.stock;
+
+public @interface GeneratedValue {
+
+	String strategy();
+
+}
---
+++ Practicals/CountryTableMySQL/HibernateExample/src/main/java/com/many/stock/Table.java
@@ -0,0 +1,9 @@
+package com.many.stock;
+
+public @interface Table {
+
+	String name();
+
+	String catalog();
+
+}
